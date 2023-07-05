13be0bfe2d952b0f55325d3674c1d73a0a4a585e
KediDuro <kedi_duro@universitetipolis.edu.al>
2023-07-05 22:03:48 +0200

    kedi new divide-n1-by-n2

--- src/main/java/al/polis/calculator/controller/CalculatorController.java
+++ src/main/java/al/polis/calculator/controller/CalculatorController.java
@@ -22,22 +22,25 @@ public class CalculatorController {
      * Create a controller to multiply two numbers
      * Request is: /multiply-two-numbers
      * @author Kedi
+     * @param dto
+     * @return 
      * 
      */
-      @PostMapping("/multiply-two-numbers")
-    public String multiply(@RequestBody AddTwoNumbersReqDto dto) {
+     @PostMapping("/multiply-two-numbers")
+     public String multiply(@RequestBody AddTwoNumbersReqDto dto) {
         System.out.println("Message received!");
         System.out.println("Parameter 1 is " + dto.getNumber1());
         System.out.println("Parameter 2 is " + dto.getNumber2());
         double result = dto.getNumber1() * dto.getNumber2();
-         System.out.println("The result is " + result);
+        System.out.println("The result is " + result);
         return "The result of " + dto.getNumber1() + " + " + dto.getNumber2() + " is " + result;
     }
-    
     /**
      * Create a controller to subtract two numbers
      * Request is: /subtract-n1-from-n2
-     * @author Henrik
+     * @author Henrik 
+     * @param dto 
+     * @return  
      */
     @PostMapping("/subtract-n1-from-n2")
     public String subtract(@RequestBody AddTwoNumbersReqDto dto) {
@@ -47,15 +50,28 @@ public class CalculatorController {
         double result = dto.getNumber1() - dto.getNumber2();
         System.out.println("The result is " + result);
         return "The result of " + dto.getNumber1() + " + " + dto.getNumber2() + " is " + result;
-    }
+   }
+
     
-    /**
+  /**
      * Create a controller to divide n1 by n2
      * Request is: /divide-n1-by-n2
-     * @author Kedi
+     * @author Kedi 
      * 
      */
-     
+      @PostMapping("/divide-n1-by-n2")
+    public String divide(@RequestBody AddTwoNumbersReqDto dto) {
+        System.out.println("Message received!");
+        System.out.println("Parameter 1 is " + dto.getNumber1());
+        System.out.println("Parameter 2 is " + dto.getNumber2());
+        double result = dto.getNumber1() / dto.getNumber2();
+         System.out.println("The result is " + result);
+        return "The result of " + dto.getNumber1() + " + " + dto.getNumber2() + " is " + result;
+    }
+    
+ 
+
+    
     /**
      * Create a controller to calculate the remainder n1 % n2
      * Request is: /remainder-n1-by-n2
