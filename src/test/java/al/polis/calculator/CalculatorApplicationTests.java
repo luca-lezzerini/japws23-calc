2d817865fd7de4d590bbd58a58363855da78b5dc
ceda-dev-01 <l.lezzerini@coopceda.it>
2023-07-05 12:07:08 +0200

    Empty project from initializr

---
+++ src/test/java/al/polis/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package al.polis.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
