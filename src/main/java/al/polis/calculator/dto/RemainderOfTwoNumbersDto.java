ef9fbb0eea76f3de2bca7260aa5df5db7ff175d1
henryk001 <hkrosi70@icloud.com>
2023-07-06 17:59:16 +0200

    Changed

---
+++ src/main/java/al/polis/calculator/dto/RemainderOfTwoNumbersDto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package al.polis.calculator.dto;
+
+import lombok.Data;
+
+/**
+ *
+ * @author henri
+ */
+@Data 
+public class RemainderOfTwoNumbersDto {
+    private double number1;
+    private double number2;
+}
