d73956b6e91ac232158e05596b8bce06c09bd6db
ceda-dev-01 <l.lezzerini@coopceda.it>
2023-07-06 15:37:26 +0200

    Solved bugs

--- src/main/java/al/polis/calculator/controller/CalculatorController.java
+++ src/main/java/al/polis/calculator/controller/CalculatorController.java
@@ -81,12 +81,12 @@ public class CalculatorController {
     @PostMapping("/remainder-n1-by-n2")
     public String remainder(@RequestBody AddTwoNumbersReqDto requestDto) {
         System.out.println("Message received!");
-        System.out.println("Parameter 1 is " + requestDto.getFirstNumber());
-        System.out.println("Parameter 2 is " + requestDto.getSecondNumber());
+        System.out.println("Parameter 1 is " + requestDto.getNumber1());
+        System.out.println("Parameter 2 is " + requestDto.getNumber2());
 
-        double result = requestDto.getFirstNumber() % requestDto.getSecondNumber();
+        double result = requestDto.getNumber1()% requestDto.getNumber2();
         System.out.println("The result is " + result);
 
-        return "The result of " + requestDto.getFirstNumber() + " % " + requestDto.getSecondNumber() + " is " + result;
+        return "The result of " + requestDto.getNumber1()+ " % " + requestDto.getNumber2()+ " is " + result;
     }
 }
