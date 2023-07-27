88db30b1bfd224ce8318a8224cb88d18e95f4e3a
henryk001 <123871266+henryk001@users.noreply.github.com>
2023-07-27 16:12:40 +0200

    kedi

--- src/main/java/al/polis/calculator/controller/CustomerController.java
+++ src/main/java/al/polis/calculator/controller/CustomerController.java
@@ -4,6 +4,7 @@
  */
 package al.polis.calculator.controller;
 
+import al.polis.calculator.repository.CustomerService;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/al/polis/calculator/repository/CustomerService.java
+++ src/main/java/al/polis/calculator/repository/CustomerService.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package al.polis.calculator.controller;
+package al.polis.calculator.repository;
 
+import al.polis.calculator.controller.Customer;
 import java.util.List;
 
 /**
